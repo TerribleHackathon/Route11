ab0a578aa15712abe9ab842b49d58e24adf62a0f
Kian Merchant <kian@silva.app>
2023-12-09 12:16:54 +1300

    wip

--- src/app/nav/page.tsx
+++ src/app/nav/page.tsx
@@ -6,10 +6,10 @@ import { ArrowRight } from 'lucide-react';
 export default function Page() {
     return (
         <div className="flex text-black font-opensans w-full h-screen flex-col md:flex-row-reverse bg-red-300">
-            <div className="flex h-1/2 md:h-screen w-screen md:w-1/2 bg-gray-300">
+            <div className="flex h-screen w-screen md:w-2/3 bg-gray-300">
                 {/* GPS // top // right */}
             </div>
-            <div className="flex flex-col h-1/2 md:h-screen w-screen md:w-1/2 bg-white">
+            <div className="flex flex-col rounded-t-2xl absolute bottom-0 bg-white w-screen h-1/2">
                 {/* bottom // left */}
 
                 <Formik
@@ -36,30 +36,46 @@ export default function Page() {
                         isSubmitting,
                         /* and other goodies */
                     }) => (
-                        <form className="flex flex-col" onSubmit={handleSubmit}>
-                            <div className="flex mx-auto">
-                                <input
-                                    type="email"
-                                    name="email"
-                                    onChange={handleChange}
-                                    onBlur={handleBlur}
-                                    className="flex bg-red-300 h-[4rem] w-full"
-                                    value={values.startLocation}
-                                />
-                            </div>
+                        <form className="flex flex-col mx-[2em] my-auto" onSubmit={handleSubmit}>
+                            <input
+                                type="email"
+                                name="email"
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                                className="flex bg-red-300 rounded-md h-[4rem] w-full"
+                                value={values.startLocation}
+                            />
                             {/* {errors.email && touched.email && errors.email} */}
                             <input
                                 type="password"
                                 name="password"
-                                className="flex bg-red-300 h-[4rem] mt-[0.8rem] w-full"
+                                className="flex bg-red-300 rounded-md h-[4rem] mt-[0.8rem] w-full"
                                 onChange={handleChange}
                                 onBlur={handleBlur}
                                 value={values.endLocation}
                             />
-                            {/* {errors.password && touched.password && errors.password} */}
-                            <button className='bg-pink-300 text-white flex w-[2.8rem] h-[2.8rem] rounded-md' type="submit" disabled={isSubmitting}>
-                                <ArrowRight className="flex m-auto" />
-                            </button>
+                            <input
+                                type="password"
+                                name="password"
+                                className="flex bg-red-300 rounded-md h-[4rem] mt-[0.8rem] w-full"
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                                value={values.endLocation}
+                            />
+                            <div className="flex mt-[0.8rem]"> 
+                                <input
+                                    type="password"
+                                    name="password"
+                                    className="flex bg-red-300 rounded-md h-[4rem] w-full"
+                                    onChange={handleChange}
+                                    onBlur={handleBlur}
+                                    value={values.endLocation}
+                                />
+                                {/* {errors.password && touched.password && errors.password} */}
+                                <button className='bg-pink-300 ml-[.5rem] text-white flex w-[5.5rem] h-[4rem] rounded-md' type="submit" disabled={isSubmitting}>
+                                    <ArrowRight className="flex m-auto" height={"3rem"} />
+                                </button>
+                            </div>
                         </form>
                     )}
                 </Formik>
