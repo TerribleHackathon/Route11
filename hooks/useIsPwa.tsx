ab7317f3baaef0236cd314dc92adce2892a31612
Kian Merchant <supesu.dev@gmail.com>
2023-12-09 13:53:41 +1300

    change the logic for detecting PWA

--- hooks/useIsPwa.tsx
+++ hooks/useIsPwa.tsx
@@ -4,19 +4,29 @@ const useIsPWA = () => {
   const [isPWA, setIsPWA] = useState(false);
 
   useEffect(() => {
-    const checkIsPWA = () => {
-      if (
-        'serviceWorker' in navigator &&
-        (window.matchMedia('(display-mode: standalone)').matches ||
-          'standalone' in window.navigator && (window.navigator as any).standalone)
-      ) {
-        setIsPWA(true);
-      } else {
-        setIsPWA(false);
+    // const checkIsPWA = () => {
+    //   if (
+    //     'serviceWorker' in navigator &&
+    //     (window.matchMedia('(display-mode: standalone)').matches ||
+    //       'standalone' in window.navigator && (window.navigator as any).standalone)
+    //   ) {
+    //     setIsPWA(true);
+    //   } else {
+    //     setIsPWA(false);
+    //   }
+    // };
+    const checkServiceWorker = async () => {
+      if ('serviceWorker' in navigator) {
+        try {
+          const registration = await navigator.serviceWorker.getRegistration();
+          setIsPWA(!!registration);
+        } catch (error) {
+          console.error('Error checking service worker registration:', error);
+        }
       }
     };
 
-    checkIsPWA();
+    checkServiceWorker();
   }, []);
 
   return isPWA;
